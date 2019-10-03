8e0b0c23cb89b257bb7d8502d80df7b383c001e5
cb <futurenz@gmail.com>
2019-10-04 06:31:30 +1300

    hs-key.pem again

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '18.209.108.64',
       username: 'ubuntu',
-      privateKey: '../hs-key.pem'
+      privateKey: 'hs-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
